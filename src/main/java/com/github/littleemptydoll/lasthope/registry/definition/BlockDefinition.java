df2f00f9fe99264973f6622422297eb73e886d58
LittleEmptyDOLL <88291047+LittleEmptyDOLL@users.noreply.github.com>
2026-09-15 13:41:19 +0300

    Add block placement shape and multiblock settings

--- src/main/java/com/github/littleemptydoll/lasthope/registry/definition/BlockDefinition.java
+++ src/main/java/com/github/littleemptydoll/lasthope/registry/definition/BlockDefinition.java
@@ -1,10 +1,11 @@
 package com.github.littleemptydoll.lasthope.registry.definition;
 
+import com.github.littleemptydoll.lasthope.block.BlockPlacement;
+import com.github.littleemptydoll.lasthope.block.BlockShape;
 import com.github.littleemptydoll.lasthope.block.ModBlockProperties;
 import com.github.littleemptydoll.lasthope.client.model.ModelType;
 import com.github.littleemptydoll.lasthope.registry.category.BlockCategory;
 import com.github.littleemptydoll.lasthope.registry.definition.settings.ContainerSettings;
-import com.github.littleemptydoll.lasthope.registry.definition.settings.InventoryLayouts;
 import com.github.littleemptydoll.lasthope.registry.tag.BlockTag;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -15,22 +16,17 @@ import java.util.EnumSet;
 import java.util.function.Supplier;
 
 public final class BlockDefinition {
-    // Block
     private DeferredBlock<? extends Block> block;
-    // Категория блока
     private final BlockCategory category;
-    // Поведение
     private final ModelType modelType;
-    // Ресурсы
     private final AssetFolder assetFolder;
-    // Параметры
     private final Supplier<BlockBehaviour.Properties> properties;
-    // Tags
     private final EnumSet<BlockTag> tags;
-    // Контейнер
     private final ContainerSettings containerSettings;
+    private final BlockPlacement placement;
+    private final BlockShape shape;
+    private final MultiBlockDefinition multiBlock;
 
-    //
     private BlockDefinition(Builder builder) {
         this.category = builder.category;
         this.modelType = builder.modelType;
@@ -38,105 +34,131 @@ public final class BlockDefinition {
         this.properties = builder.properties;
         this.tags = EnumSet.copyOf(builder.tags);
         this.containerSettings = builder.containerSettings;
+        this.placement = builder.placement;
+        this.shape = builder.shape;
+        this.multiBlock = builder.multiBlock;
     }
-    //
+
     public static Builder builder() {
         return new Builder();
     }
 
     public static final class Builder {
-        //
         private BlockCategory category;
-        //
         private ModelType modelType = ModelType.SIMPLE;
-        //
         private AssetFolder assetFolder = AssetFolder.DECORATION;
-        //
         private Supplier<BlockBehaviour.Properties> properties = ModBlockProperties::decoration;
-        //
         private final EnumSet<BlockTag> tags = EnumSet.noneOf(BlockTag.class);
-        //
         private ContainerSettings containerSettings;
+        private BlockPlacement placement = BlockPlacement.none();
+        private BlockShape shape;
+        private MultiBlockDefinition multiBlock;
 
-        //
         public BlockDefinition build() {
             if (category == null) {
                 throw new IllegalStateException("Block category is required.");
             }
+            if (multiBlock != null && !(multiBlock.parts() > 1)) {
+                throw new IllegalStateException("Multiblock definition must contain more than one part.");
+            }
             return new BlockDefinition(this);
         }
-        //Присвоение категории
+
         public Builder category(BlockCategory category) {
             this.category = category;
             return this;
         }
-        //Присвоение типа модели
+
         public Builder model(ModelType modelType) {
             this.modelType = modelType;
             return this;
         }
-        //Присвоение папки ресурсов
+
         public Builder assetFolder(AssetFolder assetFolder) {
             this.assetFolder = assetFolder;
             return this;
         }
-        //Присвоение параметров
+
         public Builder properties(Supplier<BlockBehaviour.Properties> properties) {
             this.properties = properties;
             return this;
         }
-        //Присвоение контейнера
+
         public Builder container(ContainerSettings containerSettings) {
             this.containerSettings = containerSettings;
             return this;
         }
-        //Присвоение тега
+
+        public Builder placement(BlockPlacement placement) {
+            this.placement = placement;
+            return this;
+        }
+
+        public Builder shape(BlockShape shape) {
+            this.shape = shape;
+            return this;
+        }
+
+        public Builder multiBlock(MultiBlockDefinition multiBlock) {
+            this.multiBlock = multiBlock;
+            return this;
+        }
+
         public Builder tag(BlockTag tag) {
             tags.add(tag);
             return this;
         }
-        //Присвоение тегов
+
         public Builder tags(BlockTag... tags) {
             Collections.addAll(this.tags, tags);
             return this;
         }
     }
 
-    // Вызывается только BlockRegistry после регистрации DeferredBlock
     public void setBlock(DeferredBlock<? extends Block> block) {
         this.block = block;
     }
 
-    // Getter's
-    // Block
     public DeferredBlock<? extends Block> block() {
         return block;
     }
-    // Категория
+
     public BlockCategory category() {
         return category;
     }
-    // Поведение
+
     public ModelType modelType() {
         return modelType;
     }
-    // Ресурсы
+
     public AssetFolder assetFolder() {
         return assetFolder;
     }
-    // Параметры
+
     public Supplier<BlockBehaviour.Properties> properties() {
         return properties;
     }
-    // Контейнер
+
     public ContainerSettings containerSettings() {
         return containerSettings;
     }
-    // Теги
+
+    public BlockPlacement placement() {
+        return placement;
+    }
+
+    public BlockShape shape() {
+        return shape;
+    }
+
+    public MultiBlockDefinition multiBlock() {
+        return multiBlock;
+    }
+
     public EnumSet<BlockTag> tags() {
         return EnumSet.copyOf(tags);
     }
-    // Есть ли теги
+
     public boolean hasTag(BlockTag tag) {
         return tags.contains(tag);
     }
