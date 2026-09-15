e6877812b4d65d2e23e513b6bf33d30593a342b8
LittleEmptyDOLL <88291047+LittleEmptyDOLL@users.noreply.github.com>
2026-09-15 13:42:20 +0300

    Move cardboard box shape to definition

--- src/main/java/com/github/littleemptydoll/lasthope/block/decoration/CardboardBoxBlock.java
+++ src/main/java/com/github/littleemptydoll/lasthope/block/decoration/CardboardBoxBlock.java
@@ -1,18 +1,9 @@
 package com.github.littleemptydoll.lasthope.block.decoration;
 
 import com.github.littleemptydoll.lasthope.block.container.AbstractContainerBlock;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class CardboardBoxBlock extends AbstractContainerBlock {
     public CardboardBoxBlock(Properties properties) {
         super(properties);
-    }
-
-    private static final VoxelShape SHAPE = Block.box(3, 0, 1, 13, 8, 15);
-
-    @Override
-    protected VoxelShape getBlockShape() {
-        return SHAPE;
     }
 }
