04b85735dd73f0c6ccffd0781d6ebdfb08aff2dd
LittleEmptyDOLL <88291047+LittleEmptyDOLL@users.noreply.github.com>
2026-09-15 10:06:16 +0300

    Implement generic container sounds

--- src/main/java/com/github/littleemptydoll/lasthope/registry/definition/ContainerSound.java
+++ src/main/java/com/github/littleemptydoll/lasthope/registry/definition/ContainerSound.java
@@ -1,9 +1,28 @@
 package com.github.littleemptydoll.lasthope.registry.definition;
 
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+
 public enum ContainerSound {
-    BOX,
-    WOOD,
-    METAL,
-    SAFE,
-    FRIDGE
+    BOX(SoundEvents.BARREL_OPEN, SoundEvents.BARREL_CLOSE),
+    WOOD(SoundEvents.CHEST_OPEN, SoundEvents.CHEST_CLOSE),
+    METAL(SoundEvents.IRON_DOOR_OPEN, SoundEvents.IRON_DOOR_CLOSE),
+    SAFE(SoundEvents.SHULKER_BOX_OPEN, SoundEvents.SHULKER_BOX_CLOSE),
+    FRIDGE(SoundEvents.IRON_DOOR_OPEN, SoundEvents.IRON_DOOR_CLOSE);
+
+    private final SoundEvent open;
+    private final SoundEvent close;
+
+    ContainerSound(SoundEvent open, SoundEvent close) {
+        this.open = open;
+        this.close = close;
+    }
+
+    public SoundEvent open() {
+        return open;
+    }
+
+    public SoundEvent close() {
+        return close;
+    }
 }
