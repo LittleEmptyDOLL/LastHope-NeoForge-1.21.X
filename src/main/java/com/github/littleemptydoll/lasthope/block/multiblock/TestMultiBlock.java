0707e4592103a9cb9af0973e5d6b9477b50bdf96
LittleEmptyDOLL <88291047+LittleEmptyDOLL@users.noreply.github.com>
2026-09-25 19:43:50 +0300

    Add test multiblock block class

---
+++ src/main/java/com/github/littleemptydoll/lasthope/block/multiblock/TestMultiBlock.java
@@ -0,0 +1,7 @@
+package com.github.littleemptydoll.lasthope.block.multiblock;
+
+public class TestMultiBlock extends AbstractMultiBlockBlock {
+    public TestMultiBlock(Properties properties) {
+        super(properties);
+    }
+}
